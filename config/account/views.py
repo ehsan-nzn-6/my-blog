80e1ab4daf2a182273eb05185edb067af0b48e0a
ehsan-nzn-6 <ehsan.nzn.6@gmail.com>
2021-10-12 11:15:16 +0330

    profile added

--- config/account/views.py
+++ config/account/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from blog.models import Article
 from account.mixins import *
 from django.urls import reverse_lazy
+from account.models import User
 # @login_required
 # def home(request):
 #     return render(request, 'registration/home.html')
@@ -42,3 +43,14 @@ class ArticleDelete(SuperuserAccessMixins, DeleteView):
     model = Article
     template_name = 'registration/article_confirm_delete.html'
     success_url = reverse_lazy('account:home')
+
+
+class ProfileUpdate(UpdateView):
+    model = User
+    fields = ('username', 'email', 'first_name',
+              'last_name', 'special_user', 'is_author')
+    success_url = reverse_lazy('account:profile')
+    template_name = 'registration/profile.html'
+
+    def get_object(self):
+        return User.objects.get(pk=self.request.user.pk)
